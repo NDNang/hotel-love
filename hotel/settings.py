7e64f364eebb73fb4791a1075764ec3d06190c00
Nguyen-Duy-Nang <nguyenduynang1999@gmail.com>
2022-09-12 22:27:41 +0700

    update

--- hotel/settings.py
+++ hotel/settings.py
@@ -155,8 +155,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_HOST = os.environ.get("DJANGO_STATIC_HOST", "")
-STATIC_URL = STATIC_HOST + "/static/"
+# STATIC_HOST = os.environ.get("DJANGO_STATIC_HOST", "")
+STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR , 'static')
