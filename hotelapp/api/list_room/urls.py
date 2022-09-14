40bab475ade59d7df46aacd23594b5856df27ce7
Nguyen-Duy-Nang <nguyenduynang1999@gmail.com>
2022-09-14 20:21:58 +0700

    update code

--- hotelapp/api/list_room/urls.py
+++ hotelapp/api/list_room/urls.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path,include
 from . import views
 
@@ -8,4 +7,3 @@ urlpatterns = [
     path('room/<int:pk>',views.RoomIdView.as_view(),name='detail_room'),
     path('room-filter/',views.RoomFilter.as_view(),name='room_filters')
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
